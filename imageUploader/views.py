430dfe85b142f74a6b1a4fab9f4b92f1685d59f7
Kwon Ickhyun <simpnet2@simpnet2.local>
2012-04-23 20:50:35 +0900

    이미지 자르기 및 리싸이징

--- imageUploader/views.py
+++ imageUploader/views.py
@@ -1,4 +1,3 @@
-# Create your views here.
 
 from django.views.decorators.csrf import csrf_exempt, csrf_protect
 
@@ -7,8 +6,11 @@ from django.template import RequestContext, loader
 from django.http import HttpResponse, HttpResponseRedirect
 
 import hashlib
-import time
+import time, os
 
+from PIL import Image
+
+UPLOAD_DIR = './static/upload/'
 
 def index(request):
     imageList = Post.objects.all()
@@ -32,21 +34,48 @@ def uploadSuccess(request):
 
     return HttpResponse(t.render(c))
 
+
 def saveImage(file, title):
     filename = uniqueName(file, title)
-    fd = open('./static/%s' % (filename), 'wb')
-
+    fd = open(UPLOAD_DIR + filename, 'wb')
     for chunk in file.chunks():
         fd.write(chunk)
 
     fd.close()
 
-    return filename
+    resizeAndCut(filename)
+    os.remove(UPLOAD_DIR + filename)
+
+    return 'c_' + filename
+
+def resizeAndCut(imageFile):
+    im = Image.open(UPLOAD_DIR + imageFile)
+    width, height = im.size
+
+    if width == height:
+        left, upper, right, lower = 0, 0, width, height
+    elif width > height:
+        diff = (width - height) / 2
+        left, upper, right, lower = diff, 0, width-diff, height
+    else:
+        diff = (height-width) / 2
+        left, upper, right, lower = 0, diff, width, height-diff
+
+
+    box = left, upper, right, lower
+
+    size = 114, 114
+    newim = im.crop(box)
+    newim.thumbnail(size, Image.ANTIALIAS)
+    newim.save(UPLOAD_DIR + 'c_' + imageFile, "PNG")
+
 
 def uniqueName(file, title):
     filename = file._get_name()
+    name, ext = os.path.splitext(filename)
     h = hashlib.new('sha1')
-    h.update(unicode(file._get_name()))
+    h.update(filename.encode('utf8'))
     h.update(str(time.time()))
-    return h.hexdigest()[0:6]
+
+    return h.hexdigest()[0:6] + ext.lower()
     
